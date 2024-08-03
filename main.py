dbff692ec8dcc0e3e278527ab5c9f85f8186a6f7
kktsuji <31529355+kktsuji@users.noreply.github.com>
2024-08-03 15:14:55 +0900

    Optimize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-"""Notify to Slack"""
+"""Notify to webhooks of new papers on arXiv."""
 
-import datetime
+from datetime import datetime, timezone, timedelta
 import json
 import os
 
@@ -8,7 +8,8 @@ import arxiv
 import requests
 
 
-def _make_query(keywords, categories, yesterday_str):
+def _make_query(keywords, categories, day):
+    # https://info.arxiv.org/help/api/user-manual.html for query syntax
     query = "%28"
     for k in keywords:
         query += f'ti:"{k}" OR abs:"{k}" OR '
@@ -16,82 +17,85 @@ def _make_query(keywords, categories, yesterday_str):
     for c in categories:
         query += f'cat:"{c}" OR '
     query = query[:-4] + "%29"
-    query = f"{query} AND submittedDate:[{yesterday_str} TO {yesterday_str}235959]"
+    query = f"{query} AND submittedDate:[{day} TO {day}235959]"
     return query
 
 
+def _get_arxiv_results(query):
+    client = arxiv.Client()
+    search = arxiv.Search(
+        query=query,
+        sort_by=arxiv.SortCriterion.SubmittedDate,
+        max_results=100,
+    )
+    results = list(client.results(search))
+    return results
+
+
+def _make_post_contents(r):
+    text = (
+        f"Title: {r.title}\n"
+        + f"First Author: {r.authors[0]}\n"
+        + f"Published: {r.published}\n"
+        + f"Entry: {r.pdf_url.replace('pdf','abs')[:-2]}\n"
+        + f"PDF: {r.pdf_url}\n"
+        + f"Categories: {r.categories}\n"
+        + f"Abstract: {r.summary.replace('\n',' ')}\n"
+    )
+    return text
+
+
 def _requests_post(webhook_url, text):
+    tmp = "--------------\n"
     requests.post(
         webhook_url,
         data=json.dumps(
             {
-                "text": text,
+                "text": text + tmp,
             }
         ),
         timeout=5.0,
     )
 
 
-def _notify(params):
+def _exec(params):
     webhook_url = params["webhook_url"]
     keywords = params["keywords"].split(",")
     categories = params["categories"].split(",")
 
-    today = datetime.datetime.now(datetime.timezone.utc).date()
-    yesterday = today - datetime.timedelta(days=1)
-    yesterday_str = yesterday.strftime("%Y%m%d")
-    yesterday_month_str = yesterday.strftime("%b")
-    query = _make_query(keywords, categories, yesterday_str)
+    day = datetime.now(timezone.utc).date() - timedelta(days=1)
+    day_m = day.strftime("%b")
+    query = _make_query(keywords, categories, day.strftime("%Y%m%d"))
+    results = _get_arxiv_results(query)
+    num_results = len(results)
 
-    client = arxiv.Client()
-    search = arxiv.Search(
-        # https://info.arxiv.org/help/api/user-manual.html for query syntax
-        query=query,
-        sort_by=arxiv.SortCriterion.SubmittedDate,
-        max_results=100,
-    )
-    results = list(client.results(search))
-
-    text = f"New papers on {yesterday_month_str} {yesterday.day}, {yesterday.year}.\n"
+    text = f"New papers on {day_m} {day.day}, {day.year}.\n"
     text += f'Query: "{query}"\n'
 
-    if len(results) == 0:
+    if num_results == 0:
         text += "No papers found.\n"
         _requests_post(webhook_url, text)
     else:
-        text += f"{len(results)} papers found.\n"
-        text += "--------------\n"
+        text += f"{num_results} papers found.\n"
         _requests_post(webhook_url, text)
         for r in results:
-            text = ""
-            text += f"Title: {r.title}\n" \
-                + f"First Author: {r.authors[0]}\n" \
-                + f"Published: {r.published}\n" \
-                + f"Entry: {r.pdf_url.replace("pdf","abs")[:-2]}\n" \
-                + f"PDF: {r.pdf_url}\n" \
-                + f"Categories: {r.categories}\n" \
-                + f"Abstract: {r.summary.replace("\n"," ")}\n" \
-                + "--------------\n"
+            text = _make_post_contents(r)
             _requests_post(webhook_url, text)
 
 
 def lambda_handler(event, context):
     """Lambda handler for AWS Lambda."""
-    print(event)
-    _notify(event)
-    # return 'Process completed.'
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    _exec(event)
+    return {"statusCode": 200, "body": json.dumps("Process completed.")}
 
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
+
     load_dotenv(".env")
     params = {
         "webhook_url": os.getenv("WEBHOOK_URL"),
-        "keywords": os.getenv("KEYWORDS").split(","),
-        "categories": os.getenv("CATEGORIES").split(",")
+        "keywords": os.getenv("KEYWORDS"),
+        "categories": os.getenv("CATEGORIES"),
     }
-    _notify(params)
+    _exec(params)
